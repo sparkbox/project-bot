227b282cc09e515a5560f6d3e446482d8c2ee08d
colby allen <colbyallenc@gmail.com>
2019-05-06 16:54:55 -0400

    chore: eslint added/files linted

--- skills/bot_events.js
+++ skills/bot_events.js
@@ -1,9 +1,5 @@
-module.exports = function(controller) {
-
-    controller.on('user_channel_join,user_group_join', function(bot, message) {
-
-        bot.reply(message, 'Welcome, <@' + message.user + '>');
-
-    });
-
-}
+module.exports = function (controller) {
+  controller.on('user_channel_join,user_group_join', (bot, message) => {
+    bot.reply(message, `Welcome, <@${  message.user  }>`);
+  });
+};
