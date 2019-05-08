2f49bc9d1c65ff41052d7672ff8df05433a58bba
ColorfulCodes <codescolorful@gmail.com>
2019-05-08 15:14:59 -0400

    Chore: Delete unnecessary code.

--- skills/bot_events.js
+++ skills/bot_events.js
@@ -1,5 +1,5 @@
 module.exports = function (controller) {
   controller.on('user_channel_join,user_group_join', (bot, message) => {
-    bot.reply(message, `Welcome, <@${  message.user  }>`);
+    bot.reply(message, `Welcome, <@${  message.user  }>, type /project help to learn more`);
   });
 };
