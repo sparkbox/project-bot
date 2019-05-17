532ccabf93e8c6b092c583b4a1e0b52bfff6b1c9
colby allen <colbyallenc@gmail.com>
2019-05-17 00:50:25 -0400

    feat(PBOT-78): static method for instantiating an action instance/ test pass

---
+++ test/actions.test.js
@@ -0,0 +1,24 @@
+const { expect, assert } = require('chai');
+const Actions = require('../lib/actions/actions');
+const AddLinkToProjectAction = require('../lib/actions/addLinkToProjectAction');	
+const AssertionError = require('assert').AssertionError;
+
+describe('Actions Builder', ()=> {
+  it('returns an error message if parameters not a string', () => {
+    const messageText = ['add google google.com'];
+    const expectedInput = () => Actions.fromMessageText(messageText);
+  expect(expectedInput).to.throw(AssertionError)
+  })
+
+  it('returns an error message if input is undefined', () => {
+    const expectedInput = () => Actions.fromMessageText('');
+  expect(expectedInput).to.throw(TypeError, /No Term Found/);
+  })
+
+  it('returns an new instance of action term', async () => {
+    const messageText = 'add google google.com'
+    const actionInstance = Actions.fromMessageText(messageText);
+    const expectedResult = new AddLinkToProjectAction()
+  expect(actionInstance).to.deep.equal(expectedResult);
+  });
+});
