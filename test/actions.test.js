5d11c096f8767b51a7fb31ee1f6abe02d2aef33e
colby allen <colbyallenc@gmail.com>
2019-05-24 13:52:20 -0400

    chore: eslint-config-sparkbox install PBOT-84

--- test/actions.test.js
+++ test/actions.test.js
@@ -1,24 +1,24 @@
-const { expect, assert } = require('chai');
+const { expect } = require('chai');
+const { AssertionError } = require('assert');
 const Actions = require('../lib/actions/actions');
-const AddLinkToProjectAction = require('../lib/actions/addLinkToProjectAction');	
-const AssertionError = require('assert').AssertionError;
+const AddLinkToProjectAction = require('../lib/actions/addLinkToProjectAction');
 
-describe('Actions Builder', ()=> {
+describe('Actions Builder', () => {
   it('returns an error message if parameters not a string', () => {
     const messageText = ['add google google.com'];
     const expectedInput = () => Actions.fromMessageText(messageText);
-  expect(expectedInput).to.throw(AssertionError)
-  })
+    expect(expectedInput).to.throw(AssertionError);
+  });
 
   it('returns an error message if input is undefined', () => {
     const expectedInput = () => Actions.fromMessageText('');
-  expect(expectedInput).to.throw(TypeError, /No Term Found/);
-  })
+    expect(expectedInput).to.throw(TypeError, /No Term Found/);
+  });
 
   it('returns an new instance of action term', async () => {
-    const messageText = 'add google google.com'
+    const messageText = 'add google google.com';
     const actionInstance = Actions.fromMessageText(messageText);
-    const expectedResult = new AddLinkToProjectAction()
-  expect(actionInstance).to.deep.equal(expectedResult);
+    const expectedResult = new AddLinkToProjectAction();
+    expect(actionInstance).to.deep.equal(expectedResult);
   });
 });
