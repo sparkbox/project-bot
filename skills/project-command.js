e7ba52baa84f0e70f249c4ef63466ef76c95605e
colby allen <colbyallenc@gmail.com>
2019-05-30 11:00:59 -0400

    wip: event listener)

--- skills/project-command.js
+++ skills/project-command.js
@@ -1,6 +1,16 @@
+const Actions = require('../lib/actions/actions');
+const AddLinkToProjectAction = require('../lib/actions/addLinkToProjectAction');
+const AddLinkToProjectResponse = require('../lib/actions/AddLinkToProjectResponse');
+const Project = require('../lib/project');
+
 module.exports = function(controller) {
-  // simple answer
   controller.on('slash_command', function (bot, message) {
-    bot.reply(message, 'A link has been added');
+    if(message.command === '/project'){
+      let text = message.text;
+      let command = message.command;
+      let action = Actions.fromMessageText(text);
+      let response = action.execute(text);
+      console.log(response.sendToBot(bot, message));
+    }
   });
 }
