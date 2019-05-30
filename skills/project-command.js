4535495dc053057813ef0a4a0c055344f301ed98
ColorfulCodes <codescolorful@gmail.com>
2019-05-30 13:59:45 -0400

    chore:deleted unneeded files

--- skills/project-command.js
+++
@@ -1,6 +0,0 @@
-module.exports = function(controller) {
-  // simple answer
-  controller.on('slash_command', function (bot, message) {
-    bot.reply(message, 'A link has been added');
-  });
-}
--- skills/projectCommand.js
+++ skills/projectCommand.js
@@ -5,17 +5,17 @@ const Actions = require('../lib/actions/actions.js');
 // const AddLinkToProjectResponse = require('../lib/actions/AddLinkToProjectResponse.js');
 
 
-module.exports = function(controller) {
+module.exports = async function(controller) {
   controller.on('slash_command', function (bot, message) {
     if(message.command === '/project'){
       // "find channel and excute action"
       console.log(message.channel_id)
-      if(message.channel_id === 'DK3H5J1B7'){
-        bot.reply("Channel ID")
+      if(message.channel_id === 'DK3H5J1B7') {
+        // bot.reply(message,"Channel ID")
         console.log(message.text)
-        // let find = Project.findByChannel(message.channel_id);
+        let find = await Project.findByChannel(message.channel_id);
       }
-      // let action = Actions.fromMessageText(message.text);
+      let action = Actions.fromMessageText(message.text);
       // let response = action.execute(message.text).then(console.log);
       // response.sendToBot(bot, message);
     }
