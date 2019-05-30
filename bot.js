8dd0da3f942e09618a30b9c5480d5e217786ecf6
ColorfulCodes <codescolorful@gmail.com>
2019-05-30 14:30:08 -0400

    feat: needed filter to run bot.js

--- bot.js
+++ bot.js
@@ -123,9 +123,11 @@ if (!process.env.clientId || !process.env.clientSecret) {
   require(__dirname + '/components/plugin_glitch.js')(controller);
 
   var normalizedPath = require("path").join(__dirname, "skills");
-  require("fs").readdirSync(normalizedPath).forEach(function(file) {
-    require("./skills/" + file)(controller);
-  });
+  require("fs").readdirSync(normalizedPath)
+.filter(file => file !== '.DS_Store')
+.forEach(function(file) {
+  require("./skills/" + file)(controller);
+});
 
   // This captures and evaluates any message sent to the bot as a DM
   // or sent to the bot in the form "@bot message" and passes it to
