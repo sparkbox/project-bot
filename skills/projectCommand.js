419217a156f91735cefa3264fe867bd50af2bdc8
ColorfulCodes <codescolorful@gmail.com>
2019-05-31 11:41:56 -0400

    chore: made function asynchronous and debugged

--- skills/projectCommand.js
+++ skills/projectCommand.js
@@ -1,28 +1,18 @@
-// import the class , and call it with the function
 const Project = require('../lib/project.js');
-// const AddLinkToProjectAction = require('../lib/actions/addLinkToProjectAction.js');
+const AddLinkToProjectAction = require('../lib/actions/addLinkToProjectAction.js');
 const Actions = require('../lib/actions/actions.js');
-// const AddLinkToProjectResponse = require('../lib/actions/AddLinkToProjectResponse.js');
+const AddLinkToProjectResponse = require('../lib/actions/AddLinkToProjectResponse.js');
 
 
 module.exports = function(controller) {
-  controller.on('slash_command', function (bot, message) {
+  controller.on('slash_command', async function (bot, message) {
     if(message.command === '/project'){
-      // "find channel and excute action"
-      console.log(message.channel_id)
-      console.log(message.text)
-      if(message.channel_id === 'CK5B7E5FZ') {
-        // bot.reply(message,"Channel ID")
-        console.log(message.text)
-        let find = Project.findByChannel(message.channel_id);
+        let channel = Project.findByChannel(message.channel_id);
         // this just takes text arguments (add goog google.com)
         let action = Actions.fromMessageText(message.text);
-        let links = AddLinkToProjectAction.parseParams(action);
-        // will just return promise (call sendToBot in then or use await )
-        let response = await AddLinkToProjectResponse()
-        // let response = action.execute(message.text).then(console.log);
-        response.sendToBot(bot, message);
-      }
+        let response = await action.execute(message.text);
+        response.sendToBot(bot, message.channel);
+
     }
   });
 }
