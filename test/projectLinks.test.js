e82bdcb4e256508d8f853d35df8dbac5b13e3a36
colby allen <colbyallenc@gmail.com>
2019-06-06 09:42:49 -0400

    wip: addLinkToProjectAction executes projectLinks .save() test: :white_check_mark: PBOT-55

--- test/projectLinks.test.js
+++ test/projectLinks.test.js
@@ -1,31 +1,14 @@
 const { expect } = require('chai');
 const Project = require('../lib/project');
-const ProjectLinks = require('../lib/projectLinks');
+const ProjectLink = require('../lib/projectLink');
 const AddLinkToProjectResponse = require('../lib/actions/AddLinkToProjectResponse');
 
-describe('Project Links', () => {
-  it('Saves a link to the Project', () => {
-    let project = new Project();
-    const projectLinks = new ProjectLinks();
-    projectLinks.add(project, 'google', 'google.com')
-    const projectLinksList = project.links;
-    const expectedResult = { google: 'google.com' };
-    expect(projectLinksList).to.deep.equal(expectedResult)
+describe('Project Link', () => {
+  it('Has a Project, Label, and Link', () => {
+    const projectLink = { project: {}, label: 'google', link: 'google.com' }
+    const expectedResult = new ProjectLink({}, 'google', 'google.com' );
+    expect(projectLink).to.deep.equal(expectedResult)
   }); 
 
-  it('Returns Error Message on invalid adds link to Project Links', () => {
-    const projectLinks = new ProjectLinks();
-    const projectLinksList = projectLinks.add({label: 'google', link: 'google.com'});
-    const expectedResult = 'Cannot add item.';
-    expect(projectLinksList).to.deep.equal(expectedResult)
-  }); 
-
-  it('Returns Error Message on invalid adds link to Project', () => {
-    const project = new Project();
-    const projectLinks = new ProjectLinks();
-    projectLinks.add({label: 'google', link: 'google.com'});
-    const projectList = project.links;
-    const expectedResult = {};
-    expect(projectList).to.deep.equal(expectedResult)
-  });
+  xit('Saves a link to the Project', () => {}); 
 })
