9f954969176f5fdeabe23ff1257aa70e4c4abc84
Gary Tempus <gary@heysparkbox.com>
2019-06-07 14:13:37 -0400

    :shirt: style: General formatting of addLinkToProjectAction.test.js

--- test/addLinkToProjectAction.test.js
+++ test/addLinkToProjectAction.test.js
@@ -1,43 +1,42 @@
 const { expect } = require('chai');
 const AddLinkToProjectAction = require('../lib/actions/addLinkToProjectAction');
 const AddLinkToProjectResponse = require('../lib/actions/addLinkToProjectResponse');
-const Project = require('../lib/project')
-const ProjectLinks = require('../lib/projectLinks')
+const Project = require('../lib/project');
+const ProjectLinks = require('../lib/projectLinks');
 const AssertionError = require('assert').AssertionError;
 
 describe('Add Link to Project Action', ()=> {
+  it('returns a string as an object', () => {
+    const addLinkToProjectAction = new AddLinkToProjectAction();
+    const expectedResult = { label: 'google', link: 'google.com' }; 
+    expect(addLinkToProjectAction.parseParams('add google google.com')).to.deep.equal(expectedResult);
+  });
 
-    it('returns a string as an object', () => {
-        const addLinkToProjectAction = new AddLinkToProjectAction();
-        const expectedResult = { label: 'google', link: 'google.com' }; 
-        expect(addLinkToProjectAction.parseParams('add google google.com')).to.deep.equal(expectedResult);
-    })
+  it('returns an error message on invalid input', () => {
+    const addLinkToProjectAction = new AddLinkToProjectAction();
+    const parseParamsThatThrows = () => addLinkToProjectAction.parseParams('google');
+    expect(parseParamsThatThrows).to.throw(TypeError, /invalid/);
+  });
 
-    it('returns an error message on invalid input', () => {
-        const addLinkToProjectAction = new AddLinkToProjectAction();
-        const parseParamsThatThrows = () => addLinkToProjectAction.parseParams('google')
-        expect(parseParamsThatThrows).to.throw(TypeError, /invalid/);
-    })
+  it('returns an error message if parameters are undefined', () => {
+    const addLinkToProjectAction = new AddLinkToProjectAction(); 
+    const parseParamsThatThrows = () => addLinkToProjectAction.parseParams();
+    expect(parseParamsThatThrows).to.throw(TypeError, /invalid/);
+  });
 
-    it('returns an error message if parameters are undefined', () => {
-        const addLinkToProjectAction = new AddLinkToProjectAction(); 
-        const parseParamsThatThrows = () => addLinkToProjectAction.parseParams()
-        expect(parseParamsThatThrows).to.throw(TypeError, /invalid/);
-    })
+  it('returns an error message if parameters not a string', () => {
+    const addLinkToProjectAction = new AddLinkToProjectAction();
+    const parseParamsThatThrows = () => addLinkToProjectAction.parseParams(['google', 'google.com']);
+    expect(parseParamsThatThrows).to.throw(AssertionError);
+  });
 
-    it('returns an error message if parameters not a string', () => {
-        const addLinkToProjectAction = new AddLinkToProjectAction();
-        const parseParamsThatThrows = () => addLinkToProjectAction.parseParams(['google', 'google.com'])
-        expect(parseParamsThatThrows).to.throw(AssertionError);
-    })
+  xit('@asyncResolves : adds link to DB', async () => {});
 
-    xit('@asyncResolves : adds link to DB', async () => {});
-
-    it('@asyncResolves : returns an add object containing a response', async () => {
-        const action = new AddLinkToProjectAction();
-        const project = new Project();
-        const response = await action.execute('add google google.com', project);
-        const expectedResult = new AddLinkToProjectResponse('google', 'google.com');
-        expect(response).to.deep.equal(expectedResult);
-    });
+  it('@asyncResolves : returns an add object containing a response', async () => {
+    const action = new AddLinkToProjectAction();
+    const project = new Project();
+    const response = await action.execute('add google google.com', project);
+    const expectedResult = new AddLinkToProjectResponse('google', 'google.com');
+    expect(response).to.deep.equal(expectedResult);
+  });
 });
