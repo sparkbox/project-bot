52149c7b14db90ee2461ac50ea8b221f0205fb88
ColorfulCodes <codescolorful@gmail.com>
2019-06-10 16:37:14 -0400

    wip: added production env variables

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,19 @@
 if (process.env.NODE_ENV !== 'production') {
   const env = require('node-env-file');
-  env(__dirname + '/.env');
+  env(`${__dirname  }/.env`);
 }
 
 module.exports = {
   development: {
+    client: 'mysql',
+    connection: {
+      database: process.env.DBNAME,
+      user: process.env.DBUSER,
+      password: process.env.DBPASSWORD,
+      host: process.env.DBHOST,
+    },
+  },
+  production: {
     client: 'mysql',
     connection: {
       database: process.env.DBNAME,
