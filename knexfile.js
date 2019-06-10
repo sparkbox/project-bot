990527216a410ab9847d39393df375a5db186e34
ColorfulCodes <codescolorful@gmail.com>
2019-06-10 16:00:50 -0400

    feat: added NODE_ENV to run if not production

--- knexfile.js
+++ knexfile.js
@@ -1,15 +1,16 @@
-var env = require('node-env-file');
-env(__dirname + '/.env');
+if (process.env.NODE_ENV !== 'production') {
+  const env = require('node-env-file');
+  env(__dirname + '/.env');
+}
 
 module.exports = {
-
   development: {
     client: 'mysql',
     connection: {
       database: process.env.DBNAME,
       user: process.env.DBUSER,
       password: process.env.DBPASSWORD,
-      host: process.env.DBHOST
-    }
-  }
+      host: process.env.DBHOST,
+    },
+  },
 };
