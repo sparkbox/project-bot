5a10904b8acf07a83076175ae234c333c13d0f44
colby allen <colbyallenc@gmail.com>
2019-06-10 14:38:09 -0400

    wip: projectLink.save saves a link to the Project We mock the db.addLink method to ensure that by calling projectLink.save() db.addLink is corrcetly being called and passes info PBOT-89

---
+++ test/projectLinks.test.js
@@ -0,0 +1,26 @@
+const { expect } = require('chai');
+const ProjectLink = require('../lib/projectLink');
+
+describe('Project Link', () => {
+  it('Has a Project, Label, and Link', () => {
+    const projectLink = { project: {}, label: 'google', link: 'google.com' };
+    const expectedResult = new ProjectLink({}, 'google', 'google.com');
+    expect(projectLink).to.deep.equal(expectedResult);
+  });
+
+  it('Saves a link to the Project', () => {
+    let hasAdded = false;
+    const projectLink = new ProjectLink({ id: 'c17183' }, 'google', 'google.com');
+    const db = {
+      addLink(projectId, label, link) {
+        if (projectId === 'c17183' && label === 'google' && link === 'google.com') {
+          hasAdded = true;
+        }
+      },
+    };
+
+    projectLink.save(db);
+
+    expect(hasAdded).to.equal(true);
+  });
+});
