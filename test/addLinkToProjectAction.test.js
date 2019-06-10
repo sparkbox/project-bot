ac705876f711c4c3f13175d5dc68a6d65cc38de0
colby allen <colbyallenc@gmail.com>
2019-06-10 14:38:09 -0400

    feat: Saving projectlinks to the Database
    We mock the db.addLink method to ensure that by calling projectLink.save()
    db.addLink is corrcetly being called and passes info
    PBOT-89

--- test/addLinkToProjectAction.test.js
+++ test/addLinkToProjectAction.test.js
@@ -1,39 +1,72 @@
-const { expect } = require('chai');
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+
+chai.use(chaiAsPromised);
+const { expect } = chai;
 const { AssertionError } = require('assert');
+
 const AddLinkToProjectAction = require('../lib/actions/addLinkToProjectAction');
-const AddLinkToProjectResponse = require('../lib/actions/AddLinkToProjectResponse');
+const Project = require('../lib/project');
 
 describe('Add Link to Project Action', () => {
-  it('returns a string as an object', () => {
-    const addLinkToProjectAction = new AddLinkToProjectAction();
-    const expectedResult = { label: 'google', link: 'google.com' };
-    expect(addLinkToProjectAction.parseParams('google google.com')).to.deep.equal(expectedResult);
+  it('creates a ProjectLink', async () => {
+    let projectLinkProjectArg;
+    let projectLinkLabelArg;
+    let projectLinkLinkArg;
+
+    class ProjectLinkSpy {
+      constructor(project, label, link) {
+        projectLinkProjectArg = project;
+        projectLinkLabelArg = label;
+        projectLinkLinkArg = link;
+      }
+
+      // eslint-disable-next-line class-methods-use-this
+      save() {}
+    }
+
+    class NullResponse {}
+
+    const action = new AddLinkToProjectAction(ProjectLinkSpy, NullResponse);
+
+    const project = new Project();
+    // eslint-disable-next-line no-unused-vars
+    const response = await action.execute('add google google.com', project);
+
+    expect(projectLinkProjectArg).to.equal(project);
+    expect(projectLinkLabelArg).to.equal('google');
+    expect(projectLinkLinkArg).to.equal('google.com');
   });
 
-  it('returns an error message on invalid input', () => {
+  it('throws a type error if there are not three space delimited params', async () => {
     const addLinkToProjectAction = new AddLinkToProjectAction();
-    const parseParamsThatThrows = () => addLinkToProjectAction.parseParams('google');
-    expect(parseParamsThatThrows).to.throw(TypeError, /invalid/);
+    const missingTwoParameters = 'google';
+    const result = addLinkToProjectAction.execute(missingTwoParameters);
+    await expect(result).to.be.rejectedWith(TypeError);
   });
 
-  it('returns an error message if parameters are undefined', () => {
+  it('throws a type error if no params are passed', async () => {
     const addLinkToProjectAction = new AddLinkToProjectAction();
-    const parseParamsThatThrows = () => addLinkToProjectAction.parseParams();
-    expect(parseParamsThatThrows).to.throw(TypeError, /invalid/);
+    const result = addLinkToProjectAction.execute();
+    await expect(result).to.be.rejectedWith(TypeError);
   });
 
-  it('returns an error message if parameters not a string', () => {
+  it('throws an assertion error if params not a string', async () => {
     const addLinkToProjectAction = new AddLinkToProjectAction();
-    const parseParamsThatThrows = () => addLinkToProjectAction.parseParams(['google', 'google.com']);
-    expect(parseParamsThatThrows).to.throw(AssertionError);
+    const result = addLinkToProjectAction.execute(['google', 'google.com']);
+    await expect(result).to.be.rejectedWith(AssertionError);
   });
 
   xit('@asyncResolves : adds link to DB', async () => {});
 
-  it('@asyncResolves : returns an add object containing a response', async () => {
-    const addLinkToProjectAction = new AddLinkToProjectAction();
-    const response = await addLinkToProjectAction.execute('google google.com');
-    const expectedResult = new AddLinkToProjectResponse('google', 'google.com');
-    expect(response).to.deep.equal(expectedResult);
+  // Component Level Test
+  it('@component: returns an add object containing a response', async () => {
+    const action = new AddLinkToProjectAction();
+    const project = new Project();
+
+    const response = await action.execute('add google google.com', project);
+
+    expect(response.label).to.equal('google');
+    expect(response.link).to.equal('google.com');
   });
 });
---
+++ test/drivers/mysql.test.js
@@ -0,0 +1,21 @@
+const { expect } = require('chai');
+const connection = require('../../knexfile');
+// eslint-disable-next-line import/order
+const knex = require('knex')(connection.development);
+const MySQLDriver = require('../../lib/dbDriver/mysql');
+
+describe('MySQL/Knex Persistance', () => {
+  it('@integration: addLink stores an item to the db', async () => {
+    const driver = new MySQLDriver();
+    driver.addLink({ project_id: 'sqlIntegration' }, 'MySQLIntegration', 'integrations.com');
+
+    await knex.select('*')
+      .from('project_links')
+      .where({ project_id: 'sqlIntegration' })
+      .then(rows => expect(rows[0].label).to.equal('MySQLIntegration'))
+      .then(knex.select('label')
+        .from('project_links')
+        .where({ project_id: 'sqlIntegration' })
+        .del());
+  });
+});
