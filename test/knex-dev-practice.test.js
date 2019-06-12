02d24524a70aa6edd5ca8fdc96e77edbcea90073
colby allen <colbyallenc@gmail.com>
2019-06-11 21:45:42 -0400

    wip: addLinkToProjectAction.execute() persists data to db

--- test/knex-dev-practice.test.js
+++ test/knex-dev-practice.test.js
@@ -2,7 +2,7 @@ const connection = require('../knexfile');
 // eslint-disable-next-line import/order
 const knex = require('knex')(connection.development); // In real world, we should use env variables
 
-describe.only('Knex Persistance', () => {
+describe('Knex Persistance', () => {
   it('Saves Link to the db', async () => {
     knex('project_links')
       .insert({ project_id: 'd3r3', label: 'jira4', url: 'jira4.com' })
