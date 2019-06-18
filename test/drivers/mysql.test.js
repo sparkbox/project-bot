ca04ae1171ea5a465bcb791c167d6ba5ebb1b0d0
colby allen <colbyallenc@gmail.com>
2019-06-18 07:44:59 -0400

    test: addLink stores an item to the db

--- test/drivers/mysql.test.js
+++ test/drivers/mysql.test.js
@@ -2,12 +2,28 @@ const { expect } = require('chai');
 const connection = require('../../knexfile');
 // eslint-disable-next-line import/order
 const knex = require('knex')(connection.development);
+const MySQLDriver = require('../../lib/dbDriver/mysql');
 
 describe('MySQL/Knex Persistance', () => {
   before((callback) => {
     knex.migrate.latest().then(() => {
       callback();
     });
+  });
+
+  it('@integration: addLink stores an item to the db', async () => {
+    const driver = new MySQLDriver();
+    driver.addLink('sqlIntegration', 'MySQLIntegration', 'integrations.com');
+
+    await knex.select('*')
+      .from('project_links')
+      .where({ project_id: 'sqlIntegration' })
+      .then(rows => expect(rows[0].label).to.equal('MySQLIntegration'));
+
+    await knex.select('label')
+      .from('project_links')
+      .where({ label: 'MySQLIntegration' })
+      .del();
   });
 
   it('Knex stores an item to the db', async () => {
