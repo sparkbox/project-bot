4da17769368d16823bb4a2b7d34af1fd828e72d2
colby allen <colbyallenc@gmail.com>
2019-06-18 09:02:29 -0400

    wip: ProjectLink.findByProject query method

--- skills/projectCommand.js
+++ skills/projectCommand.js
@@ -1,11 +1,13 @@
 const Project = require('../lib/project.js');
 const Actions = require('../lib/actions/actions.js');
+const MySQLDriver = require('../lib/dbDriver/mysql');
 
 module.exports = (controller) => {
   controller.on('slash_command', async (bot, message) => {
     if (message.command === '/project') {
       /* eslint-disable-next-line no-unused-vars */
-      const project = await Project.findByChannel(message.channel_id);
+      const dbDriver = new MySQLDriver();
+      const project = await Project.findByChannel(message.channel_id, dbDriver);
       const action = Actions.fromMessageText(message.text);
       const response = await action.execute(message.text, { project });
       await response.sendToBot(bot, message);
