f8772d94a17e6bcd55315acdc524910377fb585f
ColorfulCodes <codescolorful@gmail.com>
2019-06-21 10:04:36 -0400

    chore: committing to save db changes

--- test/projectOverviewResponse.test.js
+++ test/projectOverviewResponse.test.js
@@ -3,17 +3,31 @@ const ProjectOverviewResponse = require('../lib/actions/projectOverviewResponse.
 
 describe('Project Overview Response', () => {
   it('returns a formatted list of links', () => {
-    const arrayOfLinks = [
+    const links = [
       { label: 'facebook', link: 'facebook.com' },
       { label: 'google', link: 'google.com' },
     ];
     const expectedResult = '1. facebook facebook.com\n2. google google.com';
+    console.log(expectedResult);
     let replyResponse = '';
     function replyMock(message, formattedResponse) {
       replyResponse = formattedResponse;
     }
-    const responseInstance = new ProjectOverviewResponse(arrayOfLinks);
+    console.log(replyResponse);
+    const responseInstance = new ProjectOverviewResponse(links);
     responseInstance.sendToBot({ reply: replyMock });
     expect(replyResponse).to.equal(expectedResult);
+  });
+
+  it('returns true when list of links is empty', () => {
+    const links = [];
+    const expectedResult = '';
+    let replyResponse = false;
+    function replyMock(message) {
+      replyResponse = message === expectedResult;
+    }
+    const responseInstance = new ProjectOverviewResponse(links);
+    responseInstance.sendToBot({ reply: replyMock }, expectedResult);
+    expect(replyResponse).to.equal(true);
   });
 });
