070297e4928993c250fbe5f1da51d770cbae4b70
colby allen <colbyallenc@gmail.com>
2019-07-12 14:18:10 -0400

    chaking bot json

--- skills/projectCommand.js
+++ skills/projectCommand.js
@@ -3,6 +3,8 @@ const Actions = require('../lib/actions/actions.js');
 
 module.exports = (controller) => {
   controller.on('slash_command', async (bot, message) => {
+    console.log('bot', bot);
+    console.log('message', message);
     if (message.command === '/project') {
       /* eslint-disable-next-line no-unused-vars */
       const project = await Project.findByChannel(message.channel_id);
